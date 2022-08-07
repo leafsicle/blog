4f8647a786e9f0fee2326aa757de59362203b3ec
Mattclear <cookem529@gmail.com>
2022-08-06 23:51:12 -0400

    structure of the app maintenance

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react"
+import App from "./App"
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test("renders The Cutting Board", () => {
+  render(<App />)
+  const headerElement = screen.getByText(/The Cutting Board/i)
+  expect(headerElement).toBeInTheDocument()
+})
